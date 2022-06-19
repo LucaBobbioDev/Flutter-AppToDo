bba7c6785aa02c5241920bb388fa19d3a6e754e6
Luca Bobbio <bobbio.luca.117@gmail.com>
2022-06-18 22:29:16 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+//Importação padrão de todos os Apps em Flutter
+import 'package:flutter/material.dart';
+
+//Importando o componente MyApp
+import './screens/myapp.dart';
+
+//Método principal para executar a aplicação
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/screens/myapp.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+//Importando o componente HomePage
+import './home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  //Identificador na árvore de Widgets
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
